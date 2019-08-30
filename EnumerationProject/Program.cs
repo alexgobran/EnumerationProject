8df5dacd380a3530d43eb0dce40feaf4e3e81321
alexgobran <54282581+alexgobran@users.noreply.github.com>
2019-08-30 14:47:52 -0400

    Add project files.

---
+++ EnumerationProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EnumerationProject {
+
+    enum GradStatus { SummCumlaude, MagnaCumlaude, Cumlaude}
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            GradStatus gs = GradStatus.MagnaCumlaude;
+            var i = (int)gs;
+
+
+
+             if(gs == GradStatus.MagnaCumlaude)
+            {
+
+            }
+           
+        }
+    }
+}
